de1c4ac7fe9bc449b5430feb9b38bbdc66aa98a6
Silas Montgomery <silas@slimtechnologies.com>
2026-05-15 10:55:23 -0400

    Updating layout and cleaning up styles

--- pages/home.php
+++ pages/home.php
@@ -6,23 +6,31 @@ $body = <<<HTML
     <p>Here you'll find a mix of projects, apps, scripts, and other resources that I've put together. You won't find any flashy designs or clickbait here. Just good (possibly silly) content that I find useful and maybe you will too.</p>
     <p>Feel free to explore the site and check out the tools I've created. If you have any questions or suggestions, don't hesitate to <a href="/contact">reach out</a>. Thanks for visiting!</p>
 </div>
-<div class="column">
-    <h2><span class="material-symbols-outlined">construction</span> Tools</h2>
-    <ul>
-        <li>
-            <a href="/tools/lump_sum_vs_payment_plan">Lump Sum vs. Payment Plan Calculator</a>
-        </li>
-    </ul>
+<div class="flex-container top page-section">
+    <div class="box">
+        <div class="box-header">
+            <span class="icon material-symbols-outlined">construction</span> Tools
+        </div>
+        <div class="box-body">
+            <ul>
+                <li>
+                    <a href="/tools/lump_sum_vs_payment_plan">Lump Sum vs. Payment Plan Calculator</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+    <div class="box">
+        <div class="box-header">
+            <span class="icon material-symbols-outlined">shield_question</span> Other Stuff
+        </div>
+        <div class="box-body">
+            <ul>
+                <li><a href="https://github.com/silasmontgomery">GitHub</a></li>
+                <li><a href="https://old-blog.reticent.net">Old Blog</a></li>
+            </ul>
+        </div>
+    </div>
 </div>
-<div class="column">
-    <h2><span class="material-symbols-outlined">shield_question</span> Other Stuff</h2>
-    <ul>
-        <li><a href="https://github.com/silasmontgomery">GitHub</a></li>
-        <li><a href="https://old-blog.reticent.net">Old Blog</a></li>
-    </ul>
-</div>
-<div class="clear"></div>
-
 
 HTML;
 ?>
--- public/index.php
+++ public/index.php
@@ -20,13 +20,14 @@ if (!isset($body)) {
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Quicksand:wght@300..700&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Sansation:ital,wght@0,300;0,400;0,700;1,300;1,400;1,700&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0" />
     <link rel="stylesheet" type="text/css" href="/style.css">
 </head>
 <body>
     <a name="top"></a>
     <div class="center-content ">
-        <div id="header">
+        <div id="header" class="flex-container middle">
             <div id="header-logo">
                 <img src="/logo.png" alt="Logo">
             </div>
@@ -50,7 +51,11 @@ if (!isset($body)) {
     <?php endif; ?>
     <div class="divider"></div>
     <div class="center-content">
-        <p id="footer">Copyright &copy; <?php echo date("Y") ?> <?php echo $config['general']['admin_name'] ?? 'Silas Montgomery'; ?>. All rights reserved. <a href="contact.php">Contact Me</a></p>
+        <div id="footer" class="flex-container">
+            <div>Copyright &copy; <?php echo date("Y") ?> <?php echo $config['general']['admin_name'] ?? 'Silas Montgomery'; ?>.</div>
+            <div>All rights reserved.</div>
+            <div><a href="contact.php">Contact Me</a></div>
+        </div>
     </div>
 </body>
 </html>
